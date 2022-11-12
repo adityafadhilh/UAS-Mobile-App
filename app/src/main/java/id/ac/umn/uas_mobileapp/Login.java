9b5fa258b9cd0e3008af7c4e6a0123f99fe75492
Aditya Fadhil <af.herindro@gmail.com>
2022-11-12 19:02:40 +0700

    Buat halaman register

--- app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
@@ -2,13 +2,32 @@ package id.ac.umn.uas_mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    EditText username, password;
+    Button login, register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.inputName);
+        password = findViewById(R.id.inputPass);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
