8483592ee8a2cf6118c3bf84dd6764c413aeaf10
Aditya Fadhil <af.herindro@gmail.com>
2022-11-12 19:11:44 +0700

    tambah sedikit logic untuk login dan register

--- app/src/main/java/id/ac/umn/uas_mobileapp/Register.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Register.java
@@ -2,13 +2,34 @@ package id.ac.umn.uas_mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+    EditText username, password, confirmPass;
+    Button batal, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.nama_pengguna);
+        password = findViewById(R.id.register_pass);
+        confirmPass = findViewById(R.id.confirm_pass);
+
+        batal = findViewById(R.id.batal);
+        register = findViewById(R.id.daftar);
+
+        batal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
