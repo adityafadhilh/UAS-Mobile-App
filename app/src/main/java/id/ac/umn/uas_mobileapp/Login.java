df2d21ca77ff8cb6c738b4aef8ade5477d7e5717
Aditya Fadhil <af.herindro@gmail.com>
2022-11-13 07:45:02 +0700

    check login (masih error)

--- app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
@@ -7,17 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
     EditText username, password;
     Button login, register;
+    String userStr, passStr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.inputName);
-        password = findViewById(R.id.inputPass);
+        username = (EditText) findViewById(R.id.inputName);
+        password = (EditText) findViewById(R.id.inputPass);
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
@@ -27,6 +31,30 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Register.class);
                 startActivity(intent);
+            }
+        });
+
+        userStr = username.getText().toString();
+        passStr = password.getText().toString();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // login data masih manual
+                if(userStr == "john doe"){
+                    if(passStr == "test"){
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("user", userStr);
+                        bundle.putString("pass", passStr);
+                        i.putExtras(bundle);
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(Login.this, "username atau password salah", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(Login.this, "username atau password salah", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
