0e1f3d8caf8edbc57157696d6ece6ac4d302ad8e
Aditya <af.herindro@gmail.com>
2022-11-14 17:00:26 +0700

    Tampilan diubah agar sesuai nexus 5

--- app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         username = (EditText) findViewById(R.id.inputName);
         password = (EditText) findViewById(R.id.inputPass);
--- app/src/main/java/id/ac/umn/uas_mobileapp/Register.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Register.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,6 +19,9 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         username = findViewById(R.id.nama_pengguna);
         password = findViewById(R.id.register_pass);
