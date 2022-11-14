8918ec32b439c415449dd4b3a349c2499fc10720
Aditya <af.herindro@gmail.com>
2022-11-14 17:53:44 +0700

    tambah file Richi

--- app/src/main/java/id/ac/umn/uas_mobileapp/MainActivity.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/MainActivity.java
@@ -1,16 +1,74 @@
 package id.ac.umn.uas_mobileapp;
 
-import static android.os.Build.VERSION_CODES;
-
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HariFragment hariFragment = new HariFragment();
+    MingguFragment mingguFragment = new MingguFragment();
+    BulanFragment bulanFragment = new BulanFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Transaksi");
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#A0E418"));
+
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,hariFragment).commit();
+
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.hari);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(1);
+
+        BadgeDrawable badge2Drawable = bottomNavigationView.getOrCreateBadge(R.id.minggu);
+        badge2Drawable.setVisible(true);
+        badge2Drawable.setNumber(7);
+
+        BadgeDrawable badge3Drawable = bottomNavigationView.getOrCreateBadge(R.id.bulan);
+        badge3Drawable.setVisible(true);
+        badge3Drawable.setNumber(30);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.hari:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,hariFragment).commit();
+                        return true;
+                    case R.id.minggu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,mingguFragment).commit();
+                        return true;
+                    case R.id.bulan:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,bulanFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/id/ac/umn/uas_mobileapp/MingguFragment.java
@@ -0,0 +1,19 @@
+package id.ac.umn.uas_mobileapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MingguFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_minggu, container, false);
+    }
+}
