d3d5db99fe9b97741b7bcc79b229568e70e37985
Aditya Fadhil Herindro <af.herindro@gmail.com>
2022-12-04 08:33:34 +0700

    Pie chart (logic masih belum)

--- app/src/main/java/id/ac/umn/uas_mobileapp/Home_fragment.java
+++ app/src/main/java/id/ac/umn/uas_mobileapp/Home_fragment.java
@@ -1,15 +1,53 @@
 package id.ac.umn.uas_mobileapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 public class Home_fragment extends Fragment {
+    PieChart pieChart;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.home_fragment, container, false);
-}
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        pieChart = view.findViewById(R.id.piechart);
+
+        setData();
+
+        return view;
+    }
+
+    private void setData() {
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        30,
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        20,
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        40,
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        10,
+                        Color.parseColor("#29B6F6")));
+
+        // To animate the pie chart
+        pieChart.startAnimation();
+    }
 }
